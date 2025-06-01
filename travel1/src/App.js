34808638750bef3ed5fb3cdb0dabedf3d5c72e28
Saptarshi Ray <saptarshibumba2003@gmail.com>
2025-06-01 11:28:08 +0530

    Add files via upload

---
+++ travel1/src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Layout from './component/layout/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return <Layout/>;
+}
+export default App;
